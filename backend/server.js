5b2a8d637898a1c640359ae2f39325024936a1cf
jurabek10 <samiev.jurabek@bk.ru>
2025-07-26 23:52:17 +0900

    feat: create server.js and connect to database

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+  connectDB();
+});
